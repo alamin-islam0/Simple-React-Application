8e2270fb3304db9693e7a45ad95b2b225b5ae512
Alamin Islam <alamin648890@gmail.com>
2025-09-22 17:58:37 +0600

    added more functionality

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,26 +1,36 @@
-import { use } from 'react';
-import Country from '../Country/Country';
-import './Countries.css'
+import { use, useState } from "react";
+import Country from "../Country/Country";
+import "./Countries.css";
 
-const Countries = ({countriesPromise}) => {
+const Countries = ({ countriesPromise }) => {
+  const [visitedCountries, setVisitedCountries] = useState([]);
 
-    const countriesData = use(countriesPromise)
-    const countries = countriesData.countries;
-    console.log(countries)
+  const handleVisitedCountries = (country) => {
+    console.log("click", country);
+  };
 
-    return (
-        <div>
-            <div>
-            <h1>Countries {countries.length}</h1>
-            <h3>Total Visited Countries: </h3>
-            </div>
-            <div className="countries-container">
-                {
-                    countries.map(country => <Country country={country} key={country.cca3}></Country>)
-                }
-            </div>
-        </div>
-    );
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+
+  return (
+    <div>
+      <div>
+        <h1>
+          Countries <span style={{ color: "green" }}>{countries.length}</span>
+        </h1>
+        <h3>Total Visited Countries: </h3>
+      </div>
+      <div className="countries-container">
+        {countries.map((country) => (
+          <Country
+            country={country}
+            key={country.cca3}
+            handleVisitedCountries={handleVisitedCountries}
+          ></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
